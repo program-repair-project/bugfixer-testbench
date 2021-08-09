124bcfcaf66e2d2b35a9f500985ac66121b466c6
Kihong Heo <kihong.heo@kaist.ac.kr>
2021-08-09 17:16:34 +0900

    [Bin] add cmd options for localizer

--- bin/test_localization.py
+++ bin/test_localization.py
@@ -14,25 +14,44 @@ bug_dict = {}
 timestamp = ''
 
 except_case_list = (  # build failure cases list
-        '2011-01-06-e7a1d5004e-3571c955b5','2011-02-19-356b619487-a3a5157286',
-        '2011-03-20-2034e14341-7f2937223d','2011-03-25-8138f7de40-3acdca4703',
-        '2011-03-26-3acdca4703-c2fe893985','2011-04-06-18d71a6f59-187eb235fe',
-        '2011-04-07-77ed819430-efcb9a71cd','2011-04-08-efcb9a71cd-6f3148db81',
-        '2011-04-30-9c285fddbb-93f65cdeac','2011-05-13-db0c957f02-8ba00176f1',
-        '2011-05-17-453c954f8a-daecb2c0f4','2011-05-21-f5a9e17f9c-964f44a280',
-        '2011-05-24-b60f6774dc-1056c57fa9','2011-10-30-c1a4a36c14-5921e73a37',
-        '2011-10-31-2e5d5e5ac6-b5f15ef561','2011-11-01-735efbdd04-e0f781f496',
-        '2011-11-01-ceac9dc490-9b0d73af1d','2011-11-01-d2881adcbc-4591498df7',
-        '2011-11-02-c1d520d19d-9b86852d6e','2011-11-02-de50e98a07-8d520d6296',
-        '2011-11-04-9da01ac6b6-7334dfd7eb','2011-11-15-236120d80e-fb37f3b20d',
-        '2011-11-16-55acfdf7bd-3c7a573a2c','2011-11-23-eca88d3064-db0888dfc1',
-        '2011-12-04-b3ad0b7af7-1d6c98a136','2011-12-10-74343ca506-52c36e60c4',
-        '2011-12-17-db63456a8d-3dc9f0abe6','2011-12-18-beda5efd41-622412d8e6',
-        '2012-01-13-583292ab22-d74a258f24','2012-01-16-f32760bd40-032d140fd6',
-        '2012-02-18-0a91432828-032bbc3164','2012-02-20-0cb26060af-eefefddc0e',
-        '2012-02-25-38b549ea2f-1923ecfe25','2012-03-02-730b54a374-1953161b8c',
-        '2012-03-04-60dfd64bf2-34fe62619d','2012-03-04-bda5ea7111-60dfd64bf2',
-        '2012-03-19-53e3467ff2-9a460497da',)
+    '2011-01-06-e7a1d5004e-3571c955b5',
+    '2011-02-19-356b619487-a3a5157286',
+    '2011-03-20-2034e14341-7f2937223d',
+    '2011-03-25-8138f7de40-3acdca4703',
+    '2011-03-26-3acdca4703-c2fe893985',
+    '2011-04-06-18d71a6f59-187eb235fe',
+    '2011-04-07-77ed819430-efcb9a71cd',
+    '2011-04-08-efcb9a71cd-6f3148db81',
+    '2011-04-30-9c285fddbb-93f65cdeac',
+    '2011-05-13-db0c957f02-8ba00176f1',
+    '2011-05-17-453c954f8a-daecb2c0f4',
+    '2011-05-21-f5a9e17f9c-964f44a280',
+    '2011-05-24-b60f6774dc-1056c57fa9',
+    '2011-10-30-c1a4a36c14-5921e73a37',
+    '2011-10-31-2e5d5e5ac6-b5f15ef561',
+    '2011-11-01-735efbdd04-e0f781f496',
+    '2011-11-01-ceac9dc490-9b0d73af1d',
+    '2011-11-01-d2881adcbc-4591498df7',
+    '2011-11-02-c1d520d19d-9b86852d6e',
+    '2011-11-02-de50e98a07-8d520d6296',
+    '2011-11-04-9da01ac6b6-7334dfd7eb',
+    '2011-11-15-236120d80e-fb37f3b20d',
+    '2011-11-16-55acfdf7bd-3c7a573a2c',
+    '2011-11-23-eca88d3064-db0888dfc1',
+    '2011-12-04-b3ad0b7af7-1d6c98a136',
+    '2011-12-10-74343ca506-52c36e60c4',
+    '2011-12-17-db63456a8d-3dc9f0abe6',
+    '2011-12-18-beda5efd41-622412d8e6',
+    '2012-01-13-583292ab22-d74a258f24',
+    '2012-01-16-f32760bd40-032d140fd6',
+    '2012-02-18-0a91432828-032bbc3164',
+    '2012-02-20-0cb26060af-eefefddc0e',
+    '2012-02-25-38b549ea2f-1923ecfe25',
+    '2012-03-02-730b54a374-1953161b8c',
+    '2012-03-04-60dfd64bf2-34fe62619d',
+    '2012-03-04-bda5ea7111-60dfd64bf2',
+    '2012-03-19-53e3467ff2-9a460497da',
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -81,6 +100,9 @@ def build_one(project, case):
     return True
 
 
+localizer_options = {"lighttpd": ["-blacklist", "configparser.c", "-j", "1"]}
+
+
 def run_one_localizer(project, case, engine):
     cmd = [f'{RUN_DOCKER_SCRIPT}', f'{project}-{case}', '-d']
     run_docker = subprocess.run(cmd)
@@ -102,8 +124,8 @@ def run_one_localizer(project, case, engine):
     # TODO: -skip_compile
     localizer_cmd = [
         'docker', 'exec', '-it', f'{docker_id}',
-        '/bugfixer/localizer/main.exe', '-engine', engine, '/experiment'
-    ]
+        '/bugfixer/localizer/main.exe', '-engine', engine
+    ] + localizer_options[project] + ['/experiment']
     localizer = subprocess.run(localizer_cmd)
     try:
         localizer.check_returncode()
