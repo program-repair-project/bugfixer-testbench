0ca433929101d28a7790cbab181cee58742851f3
Kihong Heo <kihong.heo@gmail.com>
2021-10-17 21:08:26 +0900

    [Script] add find-bic script

---
+++ script/find-bic-libtiff-2005-12-21-3b848a7-3edb9cd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MANYBUG_HASH=f5ebfca20231dfc9b6be2536a195d5d18b2cf3cd
+
+cd /experiment
+git clone https://gitlab.com/libtiff/libtiff.git
+cd libtiff
+git checkout $MANYBUG_HASH
+cp -rf /experiment/test mytest
+
+while true; do
+        current=`git log -n1 --pretty=format:"%H"`
+        parent=`git log -n1 --pretty=format:"%P"`
+        echo $parent
+        git checkout $parent
+        make -j || continue
+        cd mytest && ./tiffcp-split.sh 2>&1 | grep "Warning"
+        if [[ $? == 0 ]]; then
+                cd .. && continue
+        fi
+
+        ./tiffcp-split.sh
+        if [[ $? == 0 ]]; then
+                break
+        else
+                cd .. && continue
+        fi
+done
+
+git checkout $current
