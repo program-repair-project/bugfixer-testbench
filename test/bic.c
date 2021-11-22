a4bbdb756f71acb5bf69d0af8de72057cd834a2c
Kihong Heo <kihong.heo@gmail.com>
2021-11-22 11:41:09 +0900

    [Test] add test cases

---
+++ test/bic.c
@@ -0,0 +1,10 @@
+int main() {
+  int x = input();
+  int abs;
+  if (x < 2) {
+    abs = x * -1;
+  } else {
+    abs = x;
+  }
+  return abs;
+}
---
+++ test/origin.c
@@ -0,0 +1,10 @@
+int main() {
+  int x = input();
+  int abs;
+  if (x < 0) {
+    abs = x * -1;
+  } else {
+    abs = x;
+  }
+  return abs;
+}
