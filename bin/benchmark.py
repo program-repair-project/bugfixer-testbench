1fdea9e575e96540479f72087c2b23a50510346c
goodtaeeun <goodtaeeun@kaist.ac.kr>
2021-11-30 20:30:15 +0900

    [Bin] add bic target location to benchmark.py

--- bin/benchmark.py
+++ bin/benchmark.py
@@ -13,6 +13,16 @@ benchmark = {
 
 bic_location = {
     "libtiff": {
-        "2005-12-14-6746b87-0d3d51d": [("tiffcp.c", 746), ("tiffcp.c", 856)]
+        "2005-12-14-6746b87-0d3d51d": [("tiffcp.c", 746), ("tiffcp.c", 856)],
+        "2005-12-21-3b848a7-3edb9cd": [("tif_dirread.c", 590)],
+        "2006-02-23-b2ce5d8-207c78a": [("tif_dirwrite.c", 338)],
+        "2006-03-03-a72cf60-0a36d7f": [("tif_dirread.c", 976)],
+        "2007-08-24-827b6bc-22da1d6": [("tif_dirinfo.c", 333), ("tif_dirinfo.c", 363)],
+        "2007-11-02-371336d-865f7b2": [("tif_dirwrite.c", 346)],
+        "2008-12-30-362dee5-565eaa2": [("tif_dirwrite.c", 1677), ("thumbnail.c", 164), ("thumbnail.c", 187), ("thumbnail.c", 558), ("thumbnail.c", 599)],
+        "2009-02-05-764dbba-2e42d63": [("tiffcrop.c", 2690)],
+        "2009-06-30-b44af47-e0b51f3": [("tiffcp.c", 360)],
+        "2010-11-27-eb326f9-eec7ec0": [("tiff2pdf.c", 769)],
+        "2010-12-13-96a5fb4-bdba15c": [("tiffcrop.c", 1889), ("tiffcrop.c", 4612), ("tiffcrop.c", 4614), ("tiffcrop.c", 4615), ("tiffcrop.c", 4632)]
     }
 }
