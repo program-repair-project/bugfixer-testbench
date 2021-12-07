cc8f06c80a10a3593966d509eae342e700a134a9
goodtaeeun <goodtaeeun@kaist.ac.kr>
2021-12-07 19:04:01 +0900

    [Bin] add gmp to run smake and sparrow

--- bin/run_smake.sh
+++ bin/run_smake.sh
@@ -1,7 +1,54 @@
 #!/bin/bash
 
+OUTDIR=/experiment/$1-$2
+mkdir $OUTDIR
+mkdir $OUTDIR/bic
+mkdir $OUTDIR/parent
+
 if [[ $1 == "gmp" ]]; then
-    target_loc=""
+    target_loc="/experiment/src/sparrow/.libs/libgmp.so.10.0.1"
+    cd /experiment/src
+    make clean
+    cp -rf /experiment/src $OUTDIR/bic/src
+    yes | /bugfixer/smake/smake --init
+    /bugfixer/smake/smake -j
+    cp -r $target_loc $OUTDIR/bic/smake-out
+    rm -r /experiment/src/sparrow
+
+    cd /experiment/src/tests/mpz
+    yes | /bugfixer/smake/smake --init
+    if [[ $2 == "13420-13421" ]]; then
+      /bugfixer/smake/smake t-powm
+      cp /experiment/src/tests/mpz/sparrow/t-powm.o.i $OUTDIR/bic/smake-out
+      rm -r /experiment/src/tests/mpz/sparrow
+
+      cd /experiment/src
+      hg update -C -r 13418
+    elif [[ $2 == "14166-14167" ]]; then
+      /bugfixer/smake/smake t-gcd
+      cp /experiment/src/tests/mpz/sparrow/t-gcd.o.i $OUTDIR/bic/smake-out
+      rm -r /experiment/src/tests/mpz/sparrow
+      
+      cd /experiment/src
+      hg update -C -r 14161
+    fi
+    
+    make clean
+    cp -rf /experiment/src $OUTDIR/parent/src
+    yes | /bugfixer/smake/smake --init
+    /bugfixer/smake/smake -j
+    cp -r $target_loc $OUTDIR/parent/smake-out
+
+    cd /experiment/src/tests/mpz
+    yes | /bugfixer/smake/smake --init
+    if [[ $2 == "13420-13421" ]]; then
+      /bugfixer/smake/smake t-powm
+      cp /experiment/src/tests/mpz/sparrow/t-powm.o.i $OUTDIR/parent/smake-out
+    elif [[ $2 == "14166-14167" ]]; then
+      /bugfixer/smake/smake t-gcd
+      cp /experiment/src/tests/mpz/sparrow/t-gcd.o.i $OUTDIR/parent/smake-out
+    fi
+
 elif [[ $1 == "libtiff" ]]; then
     if [[ $2 == "2005-12-14-6746b87-0d3d51d" ]]; then
       target_loc="/experiment/src/sparrow/tools/.libs/tiffcp"
@@ -28,29 +75,26 @@ elif [[ $1 == "libtiff" ]]; then
     else
       echo "Not supported"
     fi
+
+    cd /experiment/src
+    make clean
+    cp -rf /experiment/src $OUTDIR/bic/src
+    yes | /bugfixer/smake/smake --init
+    /bugfixer/smake/smake -j
+    cp -r $target_loc $OUTDIR/bic/smake-out
+
+    ## Todo: checking out to parent differs by projects. Generalize this process.
+    git reset --hard HEAD~1
+    rm -rf sparrow
+    make clean
+    cp -rf /experiment/src $OUTDIR/parent/src
+    yes | /bugfixer/smake/smake --init
+    /bugfixer/smake/smake -j
+    cp -r $target_loc $OUTDIR/parent/smake-out
+
 elif [[ $1 == "php" ]]; then
     target_loc=""
 else
   echo "Not supported"
 fi
 
-OUTDIR=/experiment/$1-$2
-mkdir $OUTDIR
-
-mkdir $OUTDIR/bic
-cd /experiment/src
-make clean
-cp -rf /experiment/src $OUTDIR/bic/src
-/bugfixer/smake/smake --init
-/bugfixer/smake/smake -j
-cp -r $target_loc $OUTDIR/bic/smake-out
-
-## Todo: checking out to parent differs by projects. Generalize this process.
-mkdir $OUTDIR/parent
-git reset --hard HEAD~1
-rm -rf sparrow
-make clean
-cp -rf /experiment/src $OUTDIR/parent/src
-/bugfixer/smake/smake --init
-/bugfixer/smake/smake -j
-cp -r $target_loc $OUTDIR/parent/smake-out
