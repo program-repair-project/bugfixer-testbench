ba6f89c1e3d195c7e17e6dfd1fd92ce9e0e355ef
oojahooo <oojahooo@gmail.com>
2021-12-10 14:41:40 +0900

    [Bin] modify some patch locations of benchmarks

--- bin/benchmark.py
+++ bin/benchmark.py
@@ -13,7 +13,7 @@ benchmark = {
 
 bic_location = {
     "libtiff": {
-        "2005-12-14-6746b87-0d3d51d": [("tiffcp.c", 746), ("tiffcp.c", 856)],
+        "2005-12-14-6746b87-0d3d51d": [("tiffcp.c", 746), ("tiffcp.c", 855)],
         "2005-12-21-3b848a7-3edb9cd": [("tif_dirread.c", 590)],
         "2006-02-23-b2ce5d8-207c78a": [("tif_dirwrite.c", 338)],
         "2006-03-03-a72cf60-0a36d7f": [("tif_dirread.c", 976)],
@@ -24,9 +24,9 @@ bic_location = {
                                        ("thumbnail.c", 164),
                                        ("thumbnail.c", 187),
                                        ("thumbnail.c", 558),
-                                       ("thumbnail.c", 599)],
+                                       ("thumbnail.c", 600)],
         "2009-02-05-764dbba-2e42d63": [("tiffcrop.c", 2690)],
-        "2009-06-30-b44af47-e0b51f3": [("tiffcp.c", 360)],
+        "2009-06-30-b44af47-e0b51f3": [("tiffcp.c", 355)],  # for sparrow else node issue
         "2010-11-27-eb326f9-eec7ec0": [("tiff2pdf.c", 769)],
         "2010-12-13-96a5fb4-bdba15c": [("tiffcrop.c", 1889),
                                        ("tiffcrop.c", 4612),
