0b3f09a433de1785feef7de43ec049eb706c5ded
oojahooo <oojahooo@gmail.com>
2021-12-28 14:32:22 +0900

    [Bin] add -eps option

--- bin/run-blazer.py
+++ bin/run-blazer.py
@@ -26,7 +26,7 @@ def run_blazer(args, timestamp, project, case):
         BLAZER_BIN, '-timestamp', timestamp, '-default_rule_prob',
         args.default_rule_prob, '-default_rule_prob2', args.default_rule_prob2,
         '-default_edb_prob', args.default_edb_prob, '-default_obs_prob',
-        args.default_obs_prob, '-parent_dir',
+        args.default_obs_prob, '-eps', args.eps, '-parent_dir',
         os.path.join(OUTPUT_DIR, project, case, 'parent', 'sparrow-out'),
         os.path.join(OUTPUT_DIR, project, case, 'bic', 'sparrow-out')
     ]
@@ -72,6 +72,7 @@ def main():
     parser.add_argument('--default_rule_prob2', type=str, default="0.5")
     parser.add_argument('--default_edb_prob', type=str, default="0.5")
     parser.add_argument('--default_obs_prob', type=str, default="0.99")
+    parser.add_argument('--eps', type=str, default="0.01")
     parser.add_argument('--timestamp', type=str)
     args = parser.parse_args()
     run(args, args.timestamp)
