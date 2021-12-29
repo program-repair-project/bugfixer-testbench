4195072124e2c1eb789b862aade3a2dd2ee9d2da
Kihong Heo <kihong.heo@gmail.com>
2021-12-29 13:15:45 +0900

    [Bin] fix script

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -6,8 +6,8 @@ array=("0.99" "0.5" "0.01")
 for rule_prob in "${array[@]}"; do
   for eps in "${array[@]}"; do
     for obs_prob in "${array[@]}"; do
-      echo "$PROJECT_HOME/bin/run-blazer.py -p libtiff --default_rule_prob2 0.5 --eps 0.5 --default_obs_prob 0.99 --debug --timestamp 20211229-00:00:00-$rule_prob-$eps-$obs_prob"
-      $PROJECT_HOME/bin/run-blazer.py -p libtiff --default_rule_prob2 0.5 --eps 0.5 --default_obs_prob 0.99 --debug --timestamp 20211229-00:00:00-$rule_prob-$eps-$obs_prob
+      echo "$PROJECT_HOME/bin/run-blazer.py -p libtiff --default_rule_prob2 $rule_prob --eps $eps --default_obs_prob $obs_prob --debug --timestamp 20211229-00:00:00-$rule_prob-$eps-$obs_prob"
+      $PROJECT_HOME/bin/run-blazer.py -p libtiff --default_rule_prob2 $rule_prob --eps $eps --default_obs_prob $obs_prob --debug --timestamp 20211229-00:00:00-$rule_prob-$eps-$obs_prob
     done
   done
 done
