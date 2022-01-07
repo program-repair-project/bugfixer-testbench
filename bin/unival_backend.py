bc7d8f5f490afc8823fd47838b2a9011f6dcf643
oojahooo <oojahooo@gmail.com>
2022-01-07 19:19:57 +0900

    [Bin] wrap subprocess run, remove timestamp

--- bin/unival_backend.py
+++ bin/unival_backend.py
@@ -3,12 +3,11 @@
 import argparse
 import os
 import logging
-import sys
 import subprocess
-from test_localization import bug_dict, except_case_list, init
+from benchmark import benchmark
 
 PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-OUT_DIR = os.path.join(PROJECT_HOME, 'localizer-outs')
+OUT_DIR = os.path.join(PROJECT_HOME, 'output')
 
 DOCKER_IN_DIRECTORY = '/unival'
 
@@ -19,30 +18,36 @@ logging.basicConfig(
     datefmt="%H:%M:%S")
 
 
-def initialize(args):
-    if not os.path.isdir(OUT_DIR):
-        logging.error(
-            'There is no output directory. Run UniVal Frontend first.')
-        sys.exit()
-    init(args)
+def run_one(project, case):
+    def run_cmd_and_check(cmd,
+                          *,
+                          capture_output=False,
+                          text=False,
+                          stdout=None,
+                          stderr=None):
+        process = subprocess.run(cmd,
+                                 capture_output=capture_output,
+                                 text=text,
+                                 stdout=stdout,
+                                 stderr=stderr)
+        try:
+            process.check_returncode()
+        except subprocess.CalledProcessError:
+            logging.error(f'{project}-{case} failure: {" ".join(cmd)}')
+        return process
 
+    # run docker container
+    run_cmd_and_check([
+        'docker', 'run', '-it', '-d', '--name', f'{project}-{case}', 'unival',
+        '/bin/bash'
+    ],
+                      stdout=subprocess.DEVNULL,
+                      stderr=subprocess.DEVNULL)
 
-def run_one(project, case, timestamp):
-    name_of_timestamp = timestamp.replace(':', '-')
-    docker_run_cmd = [
-        'docker', 'run', '-it', '-d', '--name',
-        f'{project}-{case}-{name_of_timestamp}', 'unival', '/bin/bash'
-    ]
-    docker_run = subprocess.run(docker_run_cmd)
-    try:
-        docker_run.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f'{project}:{case} docker run failed')
-
-    docker_ps = subprocess.run(['docker', 'ps'],
-                               capture_output=True,
-                               text=True)
-    docker_ps.check_returncode()
+    # find docker container ID
+    docker_ps = run_cmd_and_check(['docker', 'ps'],
+                                  capture_output=True,
+                                  text=True)
     dockers = docker_ps.stdout.split('\n')[1:]
     docker_id = None
     for d in dockers:
@@ -50,106 +55,70 @@ def run_one(project, case, timestamp):
         container_id = column[0]
         image = column[1]
         name = column[-1]
-        if image == 'unival' and name == f'{project}-{case}-{name_of_timestamp}':
+        if image == 'unival' and name == f'{project}-{case}':
             docker_id = container_id
             break
     if not docker_id:
         logging.error(f'Cannot find container_id of {project}:{case}')
         return
 
+    # compile java codes
     for java_file in [
             'StructuredDataCollector.java', 'ProcessDataProfile.java'
     ]:
-        javac_cmd = [
-            'docker', 'exec', '-it', f'{docker_id}', 'javac', java_file
-        ]
-        javac = subprocess.run(javac_cmd)
-        try:
-            javac.check_returncode()
-        except subprocess.CalledProcessError:
-            logging.error(f'{project}:{case} java compile failed')
+        run_cmd_and_check(
+            ['docker', 'exec', '-it', docker_id, 'javac', java_file])
 
-    DATA_PATH = os.path.join(OUT_DIR, project, f'{project}:{case}',
-                             f'{timestamp}-unival')
+    # copy data files into docker container
+    DATA_PATH = os.path.join(OUT_DIR, project, case, 'unival')
 
     for data_file in ['CausalMap.txt', 'FaultCandidates.txt', 'output.txt']:
-        cp_cmd = [
+        run_cmd_and_check([
             'docker', 'cp',
             os.path.join(DATA_PATH, data_file), f'{docker_id}:/unival/'
-        ]
-        cp = subprocess.run(cp_cmd)
-        try:
-            cp.check_returncode()
-        except subprocess.CalledProcessError:
-            logging.error(f'{project}:{case} docker cp failed')
+        ])
 
-    java_cmd = [
-        'docker', 'exec', '-it', f'{docker_id}', 'java', 'ProcessDataProfile'
-    ]
-    java = subprocess.run(java_cmd)
-    try:
-        java.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f'{project}:{case} ProcessDataProfile failed')
+    # run java
+    run_cmd_and_check(
+        ['docker', 'exec', '-it', docker_id, 'java', 'ProcessDataProfile'])
 
-    mkdir_cmd = [
-        'docker', 'exec', '-it', f'{docker_id}', 'mkdir', '-p', 'NUMFL'
-    ]
-    mkdir = subprocess.run(mkdir_cmd)
-    try:
-        mkdir.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f'{project}:{case} mkdir failed')
+    # run Rscript
+    run_cmd_and_check(
+        ['docker', 'exec', '-it', docker_id, 'mkdir', '-p', 'NUMFL'])
+    run_cmd_and_check(
+        ['docker', 'exec', '-it', docker_id, 'Rscript', 'RFC.R', '0'])
 
-    r_cmd = ['docker', 'exec', '-it', f'{docker_id}', 'Rscript', 'RFC.R', '0']
-    r = subprocess.run(r_cmd)
-    try:
-        r.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f'{project}:{case} RFC failed')
+    # copy result directory to out_dir
+    run_cmd_and_check(
+        ['docker', 'cp', f'{docker_id}:/unival/', f'{DATA_PATH}/'])
 
-    cp_cmd = ['docker', 'cp', f'{docker_id}:/unival/', f'{DATA_PATH}/']
-    cp = subprocess.run(cp_cmd)
-    try:
-        cp.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f'{project}:{case} cp output failed')
-
-    stop_cmd = ['docker', 'stop', f'{docker_id}']
-    stop = subprocess.run(stop_cmd)
-    try:
-        stop.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f'Cannot stop docker container of {project}:{case}')
-    rm_cmd = ['docker', 'rm', f'{docker_id}']
-    rm = subprocess.run(rm_cmd)
-    try:
-        rm.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f'Cannot remove docker continer of {project}:{case}')
+    # docker kill
+    run_cmd_and_check(['docker', 'kill', docker_id])
 
 
 def run(args):
-    project, case, timestamp = args.project, args.case, args.timestamp
+    project, case = args.project, args.case
     if project:
         if case:
-            run_one(project, case, timestamp)
+            run_one(project, case)
         else:
-            for case in bug_dict[project]:
-                run_one(project, case, timestamp)
+            for case in benchmark[project]:
+                run_one(project, case)
     else:
-        for project in bug_dict:
-            for case in bug_dict[project]:
-                run_one(project, case, timestamp)
+        for project in benchmark:
+            for case in benchmark[project]:
+                run_one(project, case)
 
 
 def main():
     parser = argparse.ArgumentParser(description='UniVal Backend.')
     parser.add_argument('-p', '--project', type=str)
     parser.add_argument('-c', '--case', type=str)
-    parser.add_argument('timestamp', type=str)
     args = parser.parse_args()
-    initialize(args)
+    if not os.path.isdir(OUT_DIR):
+        logging.error(
+            'There is no output directory. Run UniVal Frontend first.')
+        return
     run(args)
 
 
