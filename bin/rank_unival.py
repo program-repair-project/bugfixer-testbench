3a93c2d65ec008cb9836cec4196c7d07392c3948
oojahooo <oojahooo@gmail.com>
2022-01-12 10:11:58 +0900

    [Bin] add rank_unival.py for print ranks of location in each UniVal result of ManyBugs case

---
+++ bin/rank_unival.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+from benchmark import benchmark, bic_location
+import subprocess
+
+PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+OUT_DIR = os.path.join(PROJECT_HOME, 'output')
+
+
+def print_one_result(project, case):
+    OBS_PATH = os.path.join(OUT_DIR, project, case, 'unival', 'unival',
+                            'observation.txt')
+    search_term_list = map(lambda l: f'{l[0]}:{l[1]}',
+                           bic_location[project][case])
+    rank = None
+    for search_term in search_term_list:
+        grep = subprocess.run(['grep', '-n', search_term, OBS_PATH],
+                              capture_output=True,
+                              text=True)
+        try:
+            grep.check_returncode()
+        except subprocess.CalledProcessError:
+            pass
+        else:
+            cand = int(grep.stdout.split(':')[0])
+            rank = cand if not rank or rank > cand else rank
+
+    wc = subprocess.run(['wc', '-l', OBS_PATH], capture_output=True, text=True)
+    try:
+        wc.check_returncode()
+    except subprocess.CalledProcessError:
+        print('There is no obs result file.')
+        return
+    total = wc.stdout.split()[0]
+
+    if not rank:
+        print(f'{project}\t{case}\t{-1:3d}\t{total}')
+    else:
+        print(f'{project}\t{case}\t{rank:3d}\t{total}')
+
+
+def print_result(args):
+    project, case = args.project, args.case
+    if project:
+        if case:
+            print_one_result(project, case)
+        else:
+            for case in benchmark[project]:
+                print_one_result(project, case)
+    else:
+        for project in benchmark:
+            for case in benchmark[project]:
+                print_one_result(project, case)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Print rank of UniVal result')
+    parser.add_argument('-p', '--project', type=str)
+    parser.add_argument('-c', '--case', type=str)
+    args = parser.parse_args()
+    print_result(args)
+
+
+if __name__ == '__main__':
+    main()
