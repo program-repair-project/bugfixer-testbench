9dbd7029b127992e3793d9e83a22237aa1618662
KAIST-JongchanPark <whdals1060@kaist.ac.kr>
2022-01-12 19:00:50 +0900

    [Bin] add transform file for php

---
+++ bin/transform_php.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -i "s/ \$pharcmd"/"/g" /experiment/src/configure
+sed -i "s/alias(\"zend_error\"),//g" /experiment/src/Zend/zend.c
+sed -i "s/zend_error_noreturn/zend_error/g" /experiment/src/Zend/*.c
+sed -i "s/zend_error_noreturn/zend_error/g" /experiment/src/Zend/*.h
+sed -i "s/zend_error_noreturn/zend_error/g" /experiment/src/ext/standard/*.c
+sed -i "s/zend_error_noreturn/zend_error/g" /experiment/src/ext/standard/*.h
+sed -i 's/"sapi\/cli\/php", "run-tests.php", "-p", //g' /experiment/tester.py 
