f3bdec3683527b0247d872bfe6aaf970d00247e2
KAIST-JongchanPark <whdals1060@kaist.ac.kr>
2022-01-15 01:37:18 +0900

    [Bin] change php test timeout from 10 to 5

--- bin/transform_php.sh
+++ bin/transform_php.sh
@@ -7,5 +7,5 @@ sed -i "s/zend_error_noreturn/zend_error/g" /experiment/src/Zend/*.h
 sed -i "s/zend_error_noreturn/zend_error/g" /experiment/src/ext/standard/*.c
 sed -i "s/zend_error_noreturn/zend_error/g" /experiment/src/ext/standard/*.h
 sed -i 's/"sapi\/cli\/php", "run-tests.php", "-p", //g' /experiment/tester.py 
-sed -i 's/60/10/g' /experiment/tester.py 
-sed -i 's/\/experiment/timeout 10 \/experiment/g' /experiment/test.sh
+sed -i 's/60/5/g' /experiment/tester.py 
+sed -i 's/\/experiment/timeout 5 \/experiment/g' /experiment/test.sh
