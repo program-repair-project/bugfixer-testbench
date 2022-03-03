67469063b70c2fa3ec300bcc3b9d6724801ae581
oojahooo <oojahooo@gmail.com>
2022-03-03 17:39:52 +0900

    [Bin] use 'TrueGround' in process_result.py, remove feedback rank, reuse score rank

--- bin/process_result.py
+++ bin/process_result.py
@@ -18,48 +18,43 @@ def open_result(project, case, timestamp, result_file):
 def get_rank_list(project, case, timestamp, result_file):
     file = open_result(project, case, timestamp, result_file)
 
-    rank = []
+    rank_list = []
     for line in file.read().splitlines()[1:]:
-        cand = line.split('\t')[-1]
-        score = float(line.split('\t')[1])
-        rank.append((cand, score))
-    return rank
+        rank, score, ground = line.split('\t')[:3]
+        rank = int(rank)
+        score = float(score)
+        rank_list.append((rank, score, ground))
+    return rank_list
 
 
-def get_answer_index(project, case, rank):
-    diff = 0  # design choice, we find exactly same loc in rank for the time being
-
-    bic_location_list = bic_location[project][case]
-
-    answer_list = [(file, t + line) for t in list(range(-diff, diff + 1))
-                   for (file, line) in bic_location_list]
-    for i, (cand, score) in enumerate(rank):
-        if (cand.split(':')[0], int(cand.split(':')[1])) in answer_list:
-            return score, i
+def get_answer_index(rank_list):
+    for rank, score, ground in rank_list:
+        if 'TrueGround' in ground:
+            return score, rank
     return 2.0, -1
 
 
-def get_same_rank(rank, answer_score):
+def get_same_rank(rank_list, answer_score):
     start = -1
 
-    for i, (cand, score) in enumerate(rank):
+    for rank, score, _ in rank_list:
         if score == answer_score:
             if start < 0:
-                start = i
+                start = rank
         elif score < answer_score:
-            return start, i - 1
+            return start, rank - 1
     #print(start, i)
-    return start, i
+    return start, len(rank_list)
 
 
 def calculate_info(rank, start, end):
-    return end + 1, end - start + 1, len(rank)
+    return end, end - start + 1, len(rank)
 
 
 def get_one_result(project, case, timestamp, result_file):
     try:
         rank_list = get_rank_list(project, case, timestamp, result_file)
-        answer_score, answer_index = get_answer_index(project, case, rank_list)
+        answer_score, answer_index = get_answer_index(rank_list)
         start, end = get_same_rank(rank_list, answer_score)
         rank, tie, total = calculate_info(rank_list, start, end)
         return rank, tie, total
@@ -97,7 +92,7 @@ def print_result(result_list):
     #print(result)
     new_result = {}
     print(
-        "Case\tInit_Rank\tInit_Tie\tInit_Total\tFeedback_Rank\tFeedback_Tie\tFeedback_Total\tScore_Rank\tScore_Tie\tScore_Total"
+        "Case\tInit_Rank\tInit_Tie\tInit_Total\tScore_Rank\tScore_Tie\tScore_Total"
     )
     for result in result_list:
         for project in result:
@@ -122,9 +117,8 @@ def main():
     parser.add_argument('-t', '--timestamp', required=True, type=str)
     args = parser.parse_args()
     result_init = get_result(args, 'init.txt')
-    result_feedback = get_result(args, 'feedback.txt')
     result_score = get_result(args, 'score.txt')
-    print_result([result_init, result_feedback, result_score])
+    print_result([result_init, result_score])
 
 
 if __name__ == '__main__':
