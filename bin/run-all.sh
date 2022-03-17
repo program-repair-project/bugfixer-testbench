ecd20c5dc533eabfde69e27f30141d3de9964211
oojahooo <oojahooo@gmail.com>
2022-03-18 03:27:22 +0900

    [Bin] update run-all.sh

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -2,10 +2,10 @@
 
 PROJECT_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 
-benchmark_array=("gmp" "libtiff" "grep" "readelf" "shntool" "sed" "tar")
+benchmark_array=("gmp" "libtiff" "php" "grep" "readelf" "shntool" "sed" "tar")
 engine_array=("none" "tarantula" "ochiai" "dstar" "unival")
-eps_array=("0.01" "0.05")
-default_rule_prob_array=("0.9" "0.8")
+eps_array=("0.01" "0.05" "0.1")
+default_rule_prob_array=("0.45" "0.5" "0.55")
 
 RESULT_FILE="$PROJECT_HOME"/result-of-run-all.txt
 
@@ -24,32 +24,25 @@ for engine in "${engine_array[@]}"; do
             elif [ "$benchmark" = "php" ] || [ "$benchmark" = "grep" ] || [ "$benchmark" = "readelf" ] || [ "$benchmark" = "sed" ] || [ "$benchmark" = "tar" ]; then
                 continue
             fi
-        fi
-        timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -p "$benchmark" -e "$engine" -g --prune_cons)
-        {
-            echo "Timestamp: $timestamp"
-            "$PROJECT_HOME"/bin/process_result.py -p "$benchmark" -t "$timestamp"
-            echo ""
-        } >>"$RESULT_FILE"
-        if [ "$engine" = "ochiai" ]; then
+        elif [ "$engine" = "ochiai" ]; then
             for eps in "${eps_array[@]}"; do
-                timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -p "$benchmark" -e "$engine" -g --prune_cons --eps "$eps")
-                {
-                    echo "Option: --eps $eps"
-                    echo "Timestamp: $timestamp"
-                    "$PROJECT_HOME"/bin/process_result.py -p "$benchmark" -t "$timestamp"
-                    echo ""
-                } >>"$RESULT_FILE"
+                for drp in "${default_rule_prob_array[@]}"; do
+                    timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -p "$benchmark" -e "$engine" -g --prune_cons --eps "$eps" --default_rule_prob "$drp")
+                    {
+                        echo "Option: --eps $eps --default_rule_prob $drp"
+                        echo "Timestamp: $timestamp"
+                        "$PROJECT_HOME"/bin/process_result.py -p "$benchmark" -t "$timestamp"
+                        echo ""
+                    } >>"$RESULT_FILE"
+                done
             done
-            for drp in "${default_rule_prob_array[@]}"; do
-                timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -p "$benchmark" -e "$engine" -g --prune_cons --default_rule_prob "$drp")
-                {
-                    echo "Option: --default_rule_prob $drp"
-                    echo "Timestamp: $timestamp"
-                    "$PROJECT_HOME"/bin/process_result.py -p "$benchmark" -t "$timestamp"
-                    echo ""
-                } >>"$RESULT_FILE"
-            done
+        else
+            timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -p "$benchmark" -e "$engine" -g --prune_cons)
+            {
+                echo "Timestamp: $timestamp"
+                "$PROJECT_HOME"/bin/process_result.py -p "$benchmark" -t "$timestamp"
+                echo ""
+            } >>"$RESULT_FILE"
         fi
     done
 done
