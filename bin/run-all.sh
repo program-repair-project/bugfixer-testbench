9e208bd5e697994f70b8a45783167bc3a7b706df
KAIST-JongchanPark <whdals1060@kaist.ac.kr>
2022-03-18 11:58:01 +0900

    [Bin] update run-all.sh

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -2,7 +2,7 @@
 
 PROJECT_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 
-engine_array=("none" "ochiai" "tarantula" "dstar" "unival")
+engine_array=("none" "tarantula" "dstar" "unival" "ochiai")
 eps_array=("0.005" "0.01" "0.05" "0.1")
 default_rule_prob2_array=("0.4" "0.45" "0.5" "0.55")
 
@@ -32,27 +32,21 @@ for engine in "${engine_array[@]}"; do
             echo ""
         } >>"$RESULT_FILE"
         pkill bingo
+        continue
     elif [ "$engine" = "ochiai" ]; then
         for eps in "${eps_array[@]}"; do
-            timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -e "$engine" -g --prune_cons --eps "$eps" --default_rule_prob 0.4)
-            {
-                echo "Option: --eps $eps --default_rule_prob2 0.40"
-                echo "Timestamp: $timestamp"
-                "$PROJECT_HOME"/bin/process_result.py -t "$timestamp"
-                echo ""
-            } >>"$RESULT_FILE"
-            pkill bingo
+            for drp2 in "${default_rule_prob2_array[@]}"; do
+                timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -e "$engine" -g --prune_cons --eps "$eps" --default_rule_prob2 "$drp2")
+                {
+                    echo "Option: --eps $eps --default_rule_prob2 $drp2"
+                    echo "Timestamp: $timestamp"
+                    "$PROJECT_HOME"/bin/process_result.py -t "$timestamp"
+                    echo ""
+                } >>"$RESULT_FILE"
+                pkill bingo
+            done
         done
-        for drp2 in "${default_rule_prob2_array[@]}"; do
-            timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -e "$engine" -g --prune_cons --eps 0.005 --default_rule_prob2 "$drp2")
-            {
-                echo "Option: --eps 0.005 --default_rule_prob2 $drp2"
-                echo "Timestamp: $timestamp"
-                "$PROJECT_HOME"/bin/process_result.py -t "$timestamp"
-                echo ""
-            } >>"$RESULT_FILE"
-            pkill bingo
-        done
+        continue
     fi
     timestamp=$("$PROJECT_HOME"/bin/run-blazer.py -e "$engine" -g --prune_cons)
     {
